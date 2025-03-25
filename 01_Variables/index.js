0a2bf3d9921523752814ad0d8a1752a002654370
Pratyush Jena <codethrottle@gmail.com>
2025-03-25 20:58:39 +0530

    01_Variables

---
+++ 01_Variables/index.js
@@ -0,0 +1,7 @@
+console.log(
+  "This code contains code for the Ultimate Javascript course video no. 1"
+);
+let a = "chiku"; // a contains 67
+console.log(a);
+
+// let 8chiku = 7; //This will throw error as variable name cannot start with a number
